c0d1ea3c11d8aa06264693522de35d4794d97fc5
Natalka Danilenko <nata1danilenko12n@gmail.com>
2019-06-14 23:58:52 +0300

    failed get Group from File .csv

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ import student.Gender;
 import student.Group;
 import student.Student;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
 
 
@@ -25,7 +29,7 @@ public class Main {
         students[4] = new Student("Oleg Vinnyk",
                 20, Gender.MAN, "KPI named by I. Sikorsky",
                 "FICT", 2, "FC22");
-        Group group = new Group(221, students);
+        Group group = new Group(221, students);/*
         System.out.println(group.toString());
         try {
             group.addStudent(new Student("Rodion Gazmanow",
@@ -58,9 +62,71 @@ public class Main {
         System.out.println("Sorted by name: " + group.toString());
 
         System.out.println(Arrays.toString(group.getToArmyNow()));
-
+*/
         group.sortByGender();
         //System.out.println("Sorted by gender: " + group.toString());
         group.writeGroupInFile();
+        File file1 = new File("/Users/nataliadanilenko/Documents/GIT/HUMAN/Group.csv");
+
+        try {
+            System.out.println("Group from file: " + createGroup(file1).toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static Group createGroup(File file1) throws IOException {
+        Group myGroup = new Group();
+        String[] str1 = readFile(file1).split("\n");
+        for (String s : str1) {
+            Student st = new Student();
+            String [] info = s.split(", ");
+            for (int i = 0; i < info.length; i++) {
+                String [] currInfo = info[i].split("=");
+                if(currInfo[0].equals("name")){
+                    st.setName(currInfo[1]);
+                }
+                if(currInfo[0].equals("age")){
+                    st.setAge(Integer.valueOf(currInfo[1]));
+                }
+                if(currInfo[0].equals("gender")){
+                    st.setGender(Gender.valueOf(currInfo[1]));
+                }
+                if(currInfo[0].equals("university")){
+                    st.setUniversity(currInfo[1]);
+                }
+                if(currInfo[0].equals("faculty")){
+                    st.setFaculty(currInfo[1]);
+                }
+                if(currInfo[0].equals("year")){
+                    st.setYear(Integer.valueOf(currInfo[1]));
+                }
+                if(currInfo[0].equals("groupName")){
+                    st.setGroupName(currInfo[1]);
+                }
+                if(currInfo[0].equals("null")){
+                    continue;
+                }
+            }
+            try {
+                myGroup.addStudent(st);
+            } catch (AddStudentException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return myGroup;
+    }
+
+    public static String readFile(File file) throws IOException {
+        BufferedReader f = new BufferedReader(new FileReader(file));
+        String str = "";
+        String out = "";
+        for (; (str = f.readLine()) != null; ) {
+            out = out + str;
+        }
+        System.out.println(out);
+        return out;
     }
 }
